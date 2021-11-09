f9b0dfe2b626a506e8b73a790af5bcc4adb8e11a
jay <dj.min43@gmail.com>
2021-11-09 10:51:00 +0900

    test api call

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from 'next'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Home: NextPage = () => {
+
+  useEffect(() => {
+    hello()
+  }, [])
+
+  const hello = async () => {
+    const result = await axios.get('/api/hello')
+    console.log(result)
+  } 
+
   return (
     <div>
       hello world
