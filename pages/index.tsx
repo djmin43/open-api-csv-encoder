0e02fbcf5c65c138dc6da308c17eac0e125d4c68
jay <dj.min43@gmail.com>
2021-11-09 11:52:29 +0900

    font size works. time to think about abstraction

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,41 +6,56 @@ import { css } from '@emotion/css'
 const Home: NextPage = () => {
 
   const [designObject, setDesignObject] = useState({
-    fontColor: '#000000'
+    fontColor: '#000000',
+    fontSize: '18px'
   })
 
-  const handleColor = (e: any) => {
-    console.log(e.target.value)
+  const handleFontColor = (e: any) => {
     const newObject = {
+      ...designObject,
       fontColor: e.target.value
+    }
+    setDesignObject(newObject)
+  }
+
+  const handleFontSize = (e: any) => {
+    const newObject = {
+      ...designObject,
+      fontSize: e.target.value
     }
     setDesignObject(newObject)
   }
 
   return (
     <div className={appBody}>
-      <h1 className={header} style={{ color: designObject.fontColor}}>
+      <h1 style={{ color: designObject.fontColor, fontSize: designObject.fontSize }}>
         hello world
       </h1>
       <div className={controlPanel}>
         <span>control panel</span>
         <div className={inputContainer}>
           <span>font color: </span>
-          <input type="radio" name="choose-color" value="#d32f2f" onChange={handleColor}/>
+          <input type="radio" name="choose-font-color" value="#d32f2f" onChange={handleFontColor}/>
           <label htmlFor="red">red</label>
-          <input type="radio" name="choose-color" value="#1976d2" onChange={handleColor}/>
+          <input type="radio" name="choose-font-color" value="#1976d2" onChange={handleFontColor}/>
           <label htmlFor="blue">blue</label>
-          <input type="radio" name="choose-color" value="#2e7d32" onChange={handleColor}/>
+          <input type="radio" name="choose-font-color" value="#2e7d32" onChange={handleFontColor}/>
           <label htmlFor="green">green</label>
-          <input type="radio" name="choose-color" value="#9c27b0" onChange={handleColor}/>
+          <input type="radio" name="choose-font-color" value="#9c27b0" onChange={handleFontColor}/>
           <label htmlFor="purple">purple</label>
         </div>
         <div className={inputContainer}>
-          <span>font color: </span>
-          <input type="radio" name="choose-color" value="#d32f2f" onChange={handleColor}/>
-          <label htmlFor="red">red</label>
-          <input type="radio" name="choose-color" value="#1976d2" onChange={handleColor}/>
-          <label htmlFor="blue">blue</label>
+          <span>font size: </span>
+          <input type="radio" name="choose-font-size" value="8px" onChange={handleFontSize}/>
+          <label htmlFor="extra-small">extra small</label>
+          <input type="radio" name="choose-font-size" value="16px" onChange={handleFontSize}/>
+          <label htmlFor="extra-small">small</label>
+          <input type="radio" name="choose-font-size" value="32px" onChange={handleFontSize}/>
+          <label htmlFor="extra-small">medium</label>
+          <input type="radio" name="choose-font-size" value="64px" onChange={handleFontSize}/>
+          <label htmlFor="extra-small">large</label>
+          <input type="radio" name="choose-font-size" value="128px" onChange={handleFontSize}/>
+          <label htmlFor="extra-large">extra large</label>
         </div>
       </div>
     </div>
@@ -70,10 +85,6 @@ const applyButton = css`
   padding: 0.5rem 1rem;
   background-color: #ffffff;
   margin: 1rem;
-`
-
-const header = css`
-  font-size: 140px;
 `
 
 export default Home
