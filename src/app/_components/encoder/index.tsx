1fb912d743bf53eda714f11e05559538c1157e85
jay <dj.min43@gmail.com>
2023-09-23 13:33:31 +0900

    get it working, need to fix some errors

--- src/app/_components/encoder/index.tsx
+++ src/app/_components/encoder/index.tsx
@@ -1,6 +1,45 @@
+"use client";
 import React from "react";
 import "./encoder-styles.scss";
 
 export const Encoder = () => {
-  return <div className="container">ff</div>;
+  const readFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader();
+    const file = e.target.files && e.target.files[0];
+    if (file) {
+      reader.readAsText(file);
+
+      reader.onload = function (event) {
+        const originalContent = event.target?.result as string;
+
+        const bomContent = "\uFEFF" + originalContent;
+
+        const blob = new Blob([bomContent], {
+          type: "text/csv;charset=utf-8",
+        });
+
+        const a = document.createElement("a");
+        a.href = URL.createObjectURL(blob);
+        a.download = "new_" + file.name; // Use the original file name
+
+        a.click();
+
+        URL.revokeObjectURL(a.href);
+      };
+
+      reader.readAsText(file, "euc-kr");
+    }
+  };
+  return (
+    <div className="container">
+      <label htmlFor="csv-file">csv file: </label>
+      <input
+        accept="text/csv"
+        type="file"
+        name="csv-file"
+        aria-label="file"
+        onChange={readFile}
+      />
+    </div>
+  );
 };
