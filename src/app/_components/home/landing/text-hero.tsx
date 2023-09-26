ac536cd01f4b1b2b539ca6d3d61259bbc81dc7b9
jay <dj.min43@gmail.com>
2023-09-26 17:48:31 +0900

    remove all func, focus on csv for now

--- src/app/_components/home/landing/text-hero.tsx
+++ src/app/_components/home/landing/text-hero.tsx
@@ -9,8 +9,8 @@ export const TextHero = () => {
         <span className={"highlight"}>{"\n"}쉽고 빠르게</span> 해결 해드릴게요.
       </h1>
       <p className={"subheader2"}>
-        공공기관에서 받은 csv 파일 한글이 깨져 보일 때, 김주임이 보낸 pdf 파일이
-        깨져 보일 때, 업무에 집중할 수 있도록 모두 해결해드릴게요.
+        거래처, 공공기관, 그리고 김주임에게서 받은 csv 파일 한글이 깨져 보일 때,
+        업무에 집중할 수 있도록 모두 해결해드릴게요.
       </p>
       <Encoder />
     </div>
